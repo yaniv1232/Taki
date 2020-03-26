9c2f80a88c089c3b87869e10c34bfcffdf7e6e23
yl1232 <yl1232@gmail.com>
2020-03-26 15:31:13 +0200

    Stage 2

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 
-p1_scr = 0
-p2_scr = 0
-deck = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10'}
-while len(deck) > 0:
-    player1 = deck.pop()
-    player2 = deck.pop()
-    p1_scr += int(player1)
-    p2_scr += int(player2)
-    print("Player 1 draw " + player1)
-    print("Player 2 draw " + player2)
 
-print("Player 1 score: " + str(p1_scr))
-print("Player 2 score: " + str(p2_scr))
-if p1_scr > p2_scr:
-    print("Player 1 won")
-elif p2_scr > p1_scr:
-    print("Player 2 won")
-else:
-    print("It's a draw")
+N = input("Enter a number: ")
+deck = set()
+score_list = list()
+for i in range(int(N)):
+    deck.add(str(i+1))
+for i in range(int(N)):
+    score_list.append(int(deck.pop()))
+    print("Player " + str(i+1) + " draw " + str(score_list[i]))
+print("Player " + str(score_list.index(max(score_list))+1) + " is the winner with " + str(max(score_list)) + " points")
+
 
 
 
